34735ba0c9d0620a604bc543e2a3137ad158b8ed
marivargascr <mvb1996.23@gmail.com>
2023-01-18 14:26:42 +0100

    Upload Routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -13,26 +13,6 @@ import {
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-import asyncHandler from 'express-async-handler'
-
-console.log('productRoutes.js');
-
-/*
-
-router.get('/'), asyncHandler (async (req, res) => {
-  console.log('routes mari')
-  const products = await Product.find({})
-  console.log('mariana: ',products)
-  res.json(products)
-})
-
-router.get('/about', function(req, res) {
-  res.send('About birds');
-});
-
-*/
-
-
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
 router.get('/top', getTopProducts)
--- server.js
+++ server.js
@@ -1,20 +1,54 @@
 import express from 'express'
+import cors  from 'cors';
 import * as dotenv from 'dotenv'
 import colors from 'colors';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js'
+
 import productsRoutes  from './routes/productRoutes.js';
 import usersRoutes  from './routes/userRoutes.js';
 import orderRoutes  from './routes/orderRoutes.js';
+// import uploadRoutes  from './routes/uploadRoutes.js';
+
+
+
 
 dotenv.config();
 await connectDB();
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Mari Backend API...'));
 
-app.use('/api/products', productsRoutes)
+app.use('/api/products', productsRoutes);
+
+app.use((req, res, next) =>{
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+//UPLOADS ROUTES
+/*
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+*/
 
 
+app.use(notFound)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000;
